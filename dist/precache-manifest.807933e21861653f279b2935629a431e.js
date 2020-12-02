09956c8da700d95b0332104e0127a89ace8fad38
Anatoliy <talyanze@gmail.com>
2020-12-03 00:32:32 +0300

    dist

---
+++ dist/precache-manifest.807933e21861653f279b2935629a431e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e68ea42453f690d16e8",
+    "url": "/admin/css/app.11e171c4.css"
+  },
+  {
+    "revision": "9220b6357b577e6faee9563429dff76d",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "7e68ea42453f690d16e8",
+    "url": "/admin/js/app.6afa504d.js"
+  },
+  {
+    "revision": "6bd3ce0a03a7b4ac3895",
+    "url": "/admin/js/chunk-2d0c1056.c469c979.js"
+  },
+  {
+    "revision": "9769aa6474992b3bb5f2",
+    "url": "/admin/js/chunk-2d0c4d49.e32acb56.js"
+  },
+  {
+    "revision": "d41eb80b6f1d9f7caeed",
+    "url": "/admin/js/chunk-2d0c9708.dc461a13.js"
+  },
+  {
+    "revision": "4fa757b268155f5e079a",
+    "url": "/admin/js/chunk-2d217357.46e21943.js"
+  },
+  {
+    "revision": "22ef9a82839697699ea5",
+    "url": "/admin/js/chunk-2d217a99.6b0c112e.js"
+  },
+  {
+    "revision": "7301b329c7d3a8412475",
+    "url": "/admin/js/chunk-2d21a3d2.c59dcfcc.js"
+  },
+  {
+    "revision": "b3c9f3a3b81b80c41b4d",
+    "url": "/admin/js/chunk-2d2259e5.1597cf14.js"
+  },
+  {
+    "revision": "8f67f1251a3e510f359b",
+    "url": "/admin/js/chunk-5c896997.26307714.js"
+  },
+  {
+    "revision": "8421b373746fa0b4abb9",
+    "url": "/admin/js/chunk-vendors.becd04f3.js"
+  },
+  {
+    "revision": "ad1c7122216ff6e919bd68f90e80d94d",
+    "url": "/admin/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/admin/robots.txt"
+  }
+]);
