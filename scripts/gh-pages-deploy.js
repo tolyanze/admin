7b2dd1250f16e07bbad5d20795fccd48caf4a71b
Anatoliy <talyanze@gmail.com>
2020-12-02 10:59:50 +0300

    deploy 02/12/20

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,17 +3,17 @@ const execa = require("execa");
 
   (async () => {
     try {
-      await execa("git", ["checkout", "--orphan", "gh-page"]);
+      await execa("git", ["checkout", "--orphan", "gh-pagen"]);
       console.log("Building...");
       await execa("npm", ["run", "build"]);
       // Understand if it's dist or build folder
       const folderName = fs.existsSync("dist") ? "dist" : "build";
       await execa("git", ["--work-tree", folderName, "add", "--all"]);
-      await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-page"]);
-      console.log("Pushing to gh-page...");
-      await execa("git", ["push", "origin", "HEAD:gh-page", "--force"]);
+      await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pagen"]);
+      console.log("Pushing to gh-pagen...");
+      await execa("git", ["push", "origin", "HEAD:gh-pagen", "--force"]);
       await execa("git", ["checkout", "-f", "master"]);
-      await execa("git", ["branch", "-D", "gh-page"]);
+      await execa("git", ["branch", "-D", "gh-pagen"]);
       console.log("Successfully deployed");
     } catch (e) {
       console.log(e.message);
